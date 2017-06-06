9482388d716a1205385f3d74898ee1e54c3a79a9
Michel <fiegelmichel@orange.fr>
2017-06-06 21:54:32 +0200

    Premier commit
    Ajout du driver mongodb-driver

---
+++ MongoNote/src/main/java/fr/dincher/fiegel/MongoNote/App.java
@@ -0,0 +1,13 @@
+package fr.dincher.fiegel.MongoNote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
