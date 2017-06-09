3233392b8db48bdc69541bc7c286697faa77dc46
Michel <fiegelmichel@orange.fr>
2017-06-09 23:18:50 +0200

    NoteDao +- terminé (pas testé)

--- MongoNote/src/main/java/fr/dincher/fiegel/MongoNote/dao/NoteDao.java
+++ MongoNote/src/main/java/fr/dincher/fiegel/MongoNote/dao/NoteDao.java
@@ -1,10 +1,12 @@
 package fr.dincher.fiegel.MongoNote.dao;
 
 import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.elemMatch;
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.regex;
+import static com.mongodb.client.model.Filters.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.bson.Document;
@@ -12,6 +14,8 @@ import org.bson.Document;
 import com.mongodb.Function;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoIterable;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 
 import fr.dincher.fiegel.MongoNote.modele.Note;
 import fr.dincher.fiegel.MongoNote.modele.User;
@@ -42,11 +46,54 @@ public class NoteDao {
 		ArrayList<Note> res = notez.into(new ArrayList<Note>());
 		return res;
 	}
+	
+	public List<Note> rechercheAllNotesFromUserByTitre(User user, String titre) {
+		MongoIterable<Note> notez = notes.find(and(eq(Note.userId, user.getId()), regex(Note.contenu, ".*"+titre+".*"))).map(mapper);
+		ArrayList<Note> res = notez.into(new ArrayList<Note>());
+		return res;
+	}
 
 	public List<Note> rechercheAllNotesFromUserByContenu(User user, String contenu) {
 		MongoIterable<Note> notez = notes.find(and(eq(Note.userId, user.getId()), regex(Note.contenu, ".*"+contenu+".*"))).map(mapper);
 		ArrayList<Note> res = notez.into(new ArrayList<Note>());
 		return res;
+	}
+	
+	public List<Note> rechercheAllNotesFromUserByHashtag(User user, String hashtag) {
+		MongoIterable<Note> notez = notes.find(and(eq(Note.userId, user.getId()), elemMatch(Note.contenu, eq(hashtag)))).map(mapper);
+		ArrayList<Note> res = notez.into(new ArrayList<Note>());
+		return res;
+	}
+	
+	public List<Note> rechercheAllNotesFromUserByDate(User user, Date date) {
+		MongoIterable<Note> notez = notes.find(and(eq(Note.userId, user.getId()), eq(Note.dateAjout,date))).map(mapper);
+		ArrayList<Note> res = notez.into(new ArrayList<Note>());
+		return res;
+	}
+	
+	public List<Note> rechercheAllNotesFromUserByDates(User user, Date dateDebut, Date dateFin) {
+		if(dateDebut.compareTo(dateFin)>0){
+			Date temp=dateFin;
+			dateFin=dateDebut;
+			dateDebut=dateFin;
+		}
+		MongoIterable<Note> notez = notes.find(and(eq(Note.userId, user.getId()), gte(Note.dateAjout,dateDebut),lte(Note.dateAjout,dateFin))).map(mapper);
+		ArrayList<Note> res = notez.into(new ArrayList<Note>());
+		return res;
+	}
+	
+	public void createNote(Note newDoc){
+		notes.insertOne(newDoc.toDocument());
+	}
+	
+	public void udpateNote(Note oldDoc){
+		UpdateResult res = notes.replaceOne(eq(Note.id,oldDoc.getId()), oldDoc.toDocument());
+		System.out.println("Mise à jour User => "+oldDoc+"\n\tRésultat => "+res);
+	}
+	
+	public void deleteUser(Note oldNote){
+		DeleteResult res = notes.deleteOne(eq(Note.id,oldNote.getId()));
+		System.out.println("Suppression User => "+oldNote+"\n\tRésultat => "+res);
 	}
 
 }
